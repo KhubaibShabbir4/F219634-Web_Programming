94d5dcc7a3f8481e49e8501e2fda44bcfeb389b5
Khubaib Shabir <f219634@cfd.nu.edu.pk>
2024-03-13 19:23:51 +0500

    cp 5 added

---
+++ CP5/q1/q1.js
@@ -0,0 +1,9 @@
+// This is a single-line comment
+
+/*
+    This is a multi-line comment
+    It provides additional information about the code
+*/
+
+var x = 5; // Assign 5 to variable x
+// var y = 10; This line is commented out
